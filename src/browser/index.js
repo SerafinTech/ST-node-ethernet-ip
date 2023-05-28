b79b67e625893127c6fa0eeca8af2428836da2e7
bombjackit <94737161+bombjackit@users.noreply.github.com>
2023-05-28 17:47:28 +0200

    Update index.js (#36)
    Reinforced check of undefined objects inside _parseListIdentityResponse

--- src/browser/index.js
+++ src/browser/index.js
@@ -66,47 +66,58 @@ class Browser extends EventEmitter{
 
     _setupSocketEvents() {
         this.socket.on("message", msg => {
-            const device = this._parseListIdentityResponse(msg);
-            this._addDevice(device);
+            const device = this._parseListIdentityResponse(msg);            
+            if(Object.keys(device).length !== 0)
+                this._addDevice(device);    // Device is added only if device is not empty object
         });
    
     }
 
     _parseListIdentityResponse(msg) {
 
-        const messageData = EthernetIP.encapsulation.header.parse(msg);
-    
-        const cpf = EthernetIP.encapsulation.CPF.parse(messageData.data);
-        const data = cpf[0].data;
         let response = {};
-        let ptr = 0;
 
-        response.EncapsulationVersion = data.readUInt16LE(ptr);
-        ptr += 2;
-        response.socketAddress = {};
-        response.socketAddress.sin_family = data.readUInt16BE(ptr);
-        ptr += 2;
-        response.socketAddress.sin_port = data.readUInt16BE(ptr);
-        ptr += 2;
-        response.socketAddress.sin_addr = data.readUInt8(ptr).toString() + "." + data.readUInt8(ptr + 1).toString() + "." + data.readUInt8(ptr + 2).toString() + "." + data.readUInt8(ptr + 3).toString();
-        ptr += 4;
-        response.socketAddress.sin_zero = data.slice(ptr,ptr + 8);
-        ptr += 8;
-        response.vendorID = data.readUInt16LE(ptr);
-        ptr += 2;
-        response.deviceType = data.readUInt16LE(ptr);
-        ptr += 2;
-        response.productCode = data.readUInt16LE(ptr);
-        ptr += 2;
-        response.revision = data.readUInt8(ptr).toString() + "." + data.readUInt8(ptr + 1).toString();
-        ptr += 2;
-        response.status = data.readUInt16LE(ptr);
-        ptr += 2;
-        response.serialNumber = "0x" + data.readUInt32LE(ptr).toString(16);
-        ptr += 4;
-        response.productName = data.slice(ptr + 1, ptr + 1 + data.readUInt8(ptr)).toString();
-        ptr += (1 + data.readUInt8(ptr));
-        response.state = data.readUInt8(ptr);
+        const messageData = EthernetIP.encapsulation.header.parse(msg);
+        
+        // Check if messageData is not undefined
+        if(messageData !== undefined) {            
+            const cpf = EthernetIP.encapsulation.CPF.parse(messageData.data);
+            // Check if cpf is not undefined
+            if(cpf !== undefined) {
+                // Check if cpf is an array
+                if(Array.isArray(cpf)) {
+                    const data = cpf[0].data;                
+                    let ptr = 0;
+
+                    response.EncapsulationVersion = data.readUInt16LE(ptr);
+                    ptr += 2;
+                    response.socketAddress = {};
+                    response.socketAddress.sin_family = data.readUInt16BE(ptr);
+                    ptr += 2;
+                    response.socketAddress.sin_port = data.readUInt16BE(ptr);
+                    ptr += 2;
+                    response.socketAddress.sin_addr = data.readUInt8(ptr).toString() + "." + data.readUInt8(ptr + 1).toString() + "." + data.readUInt8(ptr + 2).toString() + "." + data.readUInt8(ptr + 3).toString();
+                    ptr += 4;
+                    response.socketAddress.sin_zero = data.slice(ptr,ptr + 8);
+                    ptr += 8;
+                    response.vendorID = data.readUInt16LE(ptr);
+                    ptr += 2;
+                    response.deviceType = data.readUInt16LE(ptr);
+                    ptr += 2;
+                    response.productCode = data.readUInt16LE(ptr);
+                    ptr += 2;
+                    response.revision = data.readUInt8(ptr).toString() + "." + data.readUInt8(ptr + 1).toString();
+                    ptr += 2;
+                    response.status = data.readUInt16LE(ptr);
+                    ptr += 2;
+                    response.serialNumber = "0x" + data.readUInt32LE(ptr).toString(16);
+                    ptr += 4;
+                    response.productName = data.slice(ptr + 1, ptr + 1 + data.readUInt8(ptr)).toString();
+                    ptr += (1 + data.readUInt8(ptr));
+                    response.state = data.readUInt8(ptr);
+                }
+            }
+        }
 
         return response;
     }
