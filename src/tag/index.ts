a1478af73bfa105731f602872a00c10b0d744d0d
greg9504 <greg9504@gmail.com>
2024-05-04 22:45:47 -0400

    support SHORT_STRING tag type, ascii string with length at begining.  Supported by Micro850 PLC

--- src/tag/index.ts
+++ src/tag/index.ts
@@ -494,7 +494,7 @@ export class Tag extends EventEmitter {
      * @param Data - Returned from Successful Read Tag Request
      */
     parseReadMessageResponseValueForAtomic(data: Buffer) {
-        const { SINT, INT, DINT, REAL, BOOL, LINT, BIT_STRING, UINT } = Types;
+        const { SINT, INT, DINT, REAL, BOOL, LINT, BIT_STRING, UINT, SHORT_STRING } = Types;
 
         const { read_size } = this.state;
 
@@ -583,9 +583,22 @@ export class Tag extends EventEmitter {
                     this.controller_value = data.readBigInt64LE(2);
                 }
                 break;
+                case SHORT_STRING: // single byte character string
+                if (data.length > 3) {
+                    const len = data.readUInt8(2);
+                    this.controller_value = data.toString('utf8', 3); // use latin1 encoding?
+                    if (len !== this.controller_value.length) {
+                        throw new Error(` Read from Controller: Short string length mismatch`);
+                    }
+                } else {
+                    //empty string, data[2] will be 0 as well, as it is the string length
+                    this.controller_value = null;
+                }
+                break;
             default:
+                const ttt = CIP.DataTypes.getTypeCodeString(this.state.tag.type);
                 throw new Error(
-                    `Unrecognized Type Passed Read from Controller: ${this.state.tag.type}`
+                    `Unrecognized Type Passed Read from Controller: ${this.state.tag.type} ${ttt}`
                 );
         }
         /* eslint-enable indent */
@@ -600,7 +613,10 @@ export class Tag extends EventEmitter {
      * @memberof Tag
      */
     generateWriteMessageRequest(value: any = null, size: number = 0x01): Buffer {
-        if (value !== null) this.state.tag.value = value;
+        // allow null value for short string
+        if (value !== null || this.state.tag.type === Types.SHORT_STRING) {
+            this.state.tag.value = value;
+        } 
 
         const { tag } = this.state;
 
@@ -668,7 +684,7 @@ export class Tag extends EventEmitter {
      */
     generateWriteMessageRequestForAtomic(value: any, size: number) {
         const { tag } = this.state;
-        const { SINT, INT, DINT, REAL, BOOL, LINT } = Types;
+        const { SINT, INT, DINT, REAL, BOOL, LINT, SHORT_STRING } = Types;
         // Build Message Router to Embed in UCMM
         let buf = Buffer.alloc(4);
         let valBuf = null;
@@ -754,6 +770,22 @@ export class Tag extends EventEmitter {
                 }
                 buf = Buffer.concat([buf, valBuf]);
                 break;
+                case SHORT_STRING:
+                    valBuf = Buffer.alloc(1);
+                    if (!tag.value || tag.value.length === 0)
+                        valBuf.writeInt8(0x00);
+                    else {
+                        const strBuf = Buffer.from(tag.value, 'latin1');
+                        const strlen = strBuf.length;
+                        if ( strlen > 255) {
+                            throw new Error(`String too long to Write to Controller: ${tag.type}, limit 255 bytes, length ${strlen}`);
+                        }
+                        valBuf = Buffer.alloc(1 + strlen); // 1+ for length byte
+                        valBuf.writeUInt8(strlen);//write the length
+                        strBuf.copy(valBuf,1,);
+                    }
+                    buf = Buffer.concat([buf, valBuf]);
+                    break;
             default:
                 throw new Error(`Unrecognized Type to Write to Controller: ${tag.type}`);
         }
